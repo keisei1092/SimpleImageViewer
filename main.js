f202065eff6749b33d75bbca872c391281fed0e9
keisei_1092 <keiseis1092@gmail.com>
2017-08-14 09:22:36 +0900

    push

--- main.js
+++ main.js
@@ -13,7 +13,12 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 200,
+    height: 150,
+    alwaysOnTop: true,
+    frame: false
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
